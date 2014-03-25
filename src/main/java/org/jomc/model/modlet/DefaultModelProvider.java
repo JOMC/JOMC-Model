49dc9e29df82c17ad95124b4506630efd92a9ea3
Christian Schulte <cs@schulte.it>
2014-03-25 03:47:48 +0000

    o Message updates.
    See #71
    
    
    
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk/jomc-model@4940 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- src/main/java/org/jomc/model/modlet/DefaultModelProvider.java
+++ src/main/java/org/jomc/model/modlet/DefaultModelProvider.java
@@ -463,7 +463,7 @@ public class DefaultModelProvider implements ModelProvider
             if ( context.isLoggable( Level.FINE ) )
             {
                 context.log( Level.FINE, getMessage( "contextReport", count, location,
-                                                     Long.valueOf( System.currentTimeMillis() - t0 ) ), null );
+                                                     System.currentTimeMillis() - t0 ), null );
 
             }
 
